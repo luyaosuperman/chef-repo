908310d94973c5ae217f098ec6f52870970745f4
root <root@ip-172-31-0-137.ap-southeast-2.compute.internal>
2017-10-07 12:52:47 +0000

    commit

--- learn_chef_httpd/recipes/deploy.rb
+++ learn_chef_httpd/recipes/deploy.rb
@@ -7,6 +7,8 @@ app = search(:aws_opsworks_app).first
 
 file '/var/www/html/deploy.html' do
   content "deployed
-shortname - #{app['shortname']}
+shortname       - #{app['shortname']}
+app_source.type - #{app['app_source']['type']}
+app_source.url  - #{app['app_source']['url']}
 "
 end
