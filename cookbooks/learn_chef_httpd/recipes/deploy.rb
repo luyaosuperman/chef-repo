06706cffb709d617d1b7d7cec80969b5c99bbf14
root <root@ip-172-31-0-137.ap-southeast-2.compute.internal>
2017-10-07 12:43:11 +0000

    commit

--- cookbooks/learn_chef_httpd/recipes/deploy.rb
+++ cookbooks/learn_chef_httpd/recipes/deploy.rb
@@ -3,6 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+app = search(:aws_opsworks_app).first
+
 file '/var/www/html/deploy.html' do
-  content 'deployed'
+  content "deployed
+shortname - #{app['shortname']}
+"
 end
