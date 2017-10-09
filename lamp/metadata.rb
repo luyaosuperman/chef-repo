f53d002808e363a14208091490fa774e2d68302e
root <root@ip-172-31-0-137.ap-southeast-2.compute.internal>
2017-10-09 03:04:23 +0000

    commit

--- lamp/metadata.rb
+++ lamp/metadata.rb
@@ -1,20 +1,20 @@
-name 'learn_chef_httpd'
+name 'lamp'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures learn_chef_httpd'
-long_description 'Installs/Configures learn_chef_httpd'
-version '0.4.2'
+description 'Installs/Configures lamp'
+long_description 'Installs/Configures lamp'
+version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/learn_chef_httpd/issues'
+# issues_url 'https://github.com/<insert_org_here>/lamp/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/learn_chef_httpd'
+# source_url 'https://github.com/<insert_org_here>/lamp'
