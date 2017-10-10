b04bb28a0d8066a1bd15a7d6af3c51f50592728a
root <root@ip-172-31-0-137.ap-southeast-2.compute.internal>
2017-10-10 00:06:34 +0000

    commit

--- lamp/metadata.rb
+++ lamp/metadata.rb
@@ -18,3 +18,8 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/lamp'
+issues_url 'https://github.com/learn-chef/lamp/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/learn-chef/lamp' if respond_to?(:source_url)
+supports 'centos-7'
+
+depends 'httpd', '~> 0.5'
---
+++ lamp/recipes/web.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook:: lamp
+# Recipe:: web
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+directory node['lamp']['web']['document_root'] do
+  recursive true
+end
+
+directory '/var/log/apache2'
+
+httpd_config 'default' do
+  source 'default.conf.erb'
+end
+
+httpd_service 'default' do
+  mpm 'prefork'
+  action [:create, :start]
+  subscribes :restart, 'httpd_config[default]'
+end
