0b71daee516b065f8b930c86377f32c920068952
root <root@ip-172-31-0-137.ap-southeast-2.compute.internal>
2017-10-13 11:30:16 +0000

    commit

--- learn_chef_httpd/recipes/deploy.rb
+++ learn_chef_httpd/recipes/deploy.rb
@@ -24,7 +24,7 @@ stack-layer     - #{node['stack-layer']}
 
 Enviroment variables:
 
-username        - 
+username        - #{app['environment']['username']}
 
 "
 end
---
+++ testapp2.sh
@@ -0,0 +1,1 @@
+aws opsworks --region ap-southeast-2 create-deployment --stack-id f8346fd0-07ea-47d4-be55-0d3419a20045 --app-id 1b8f3bf0-ca46-467e-aba2-d170ecf99825 --command "{\"Name\":\"deploy\"}"
