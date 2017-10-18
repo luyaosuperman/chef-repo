280747c57e8f249a1a7346041db12dd56ada1a57
root <root@ip-172-31-0-137.ap-southeast-2.compute.internal>
2017-10-18 03:03:56 +0000

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 git add -A ; git commit -m "commit" ; git push;
 aws opsworks --region ap-southeast-2 create-deployment --stack-id f8346fd0-07ea-47d4-be55-0d3419a20045 --command "{\"Name\":\"update_custom_cookbooks\"}"
 #aws opsworks --region ap-southeast-2 wait deployment-successful --stack-id f8346fd0-07ea-47d4-be55-0d3419a20045
